8d5b2e8a015332b094233026ae5d1da4a6b3a3c6
Daliego <daliego82@gmail.com>
2024-10-20 21:56:37 -0300

    feat: application with  implementation of a decryption animation

--- app.js
+++ app.js
@@ -1,16 +1,73 @@
-import { generateRandomValue } from "./utils/values_generator";
+import {
+  generateRandomCharacters,
+  generateRandomValues,
+} from "./utils/values_generator.js";
+import { calculateTimeout } from "./scripts/calculate_timeout.js";
+import { onClick } from "./scripts/on_click.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const letterTags = document.getElementsByClassName("wordsToChange");
+  let isBreaking = false;
+  const letterTags = document.getElementsByClassName("wordToChange");
+  const button = document.getElementById("decryptButton");
+  const missionTitle = document.getElementsByClassName("smallTitle")[0];
+  const restarWords = ["marinheir", "d-b/tw;?/", "$z8xS)s!]"];
+  const intervalsIds = [];
 
-  for (let letterTag of letterTags) {
-    const value = letterTag.innerHTML.trim();
+  const setButtonTitle = (text) => {
+    button.innerHTML = text;
+  };
 
-    //Gerar um novo valor aleatorio
-    const randomValue = generateRandomValue();
-    console.log(randomValue);
+  const setMissionTitile = (text) => {
+    missionTitle.innerHTML = text;
+  };
 
-    //colocar o novo valor aleatorio varia vezes
-  }
-  // document.getElementsByClassName('mainWord')[0].style.color = 'blue';
+  const restartIndex = (index) => {
+    const tags = Array.prototype.slice.call(letterTags);
+    const restarWord =
+      restarWords[generateRandomValues(0, restarWords.length - 1)];
+    tags[index].innerHTML = restarWord[index];
+  };
+
+  const isLoadind = () => {
+    return isBreaking;
+  };
+
+  const toogleIsBreaking = () => {
+    isBreaking = !isBreaking;
+  };
+
+  const handleBreakingStart = () => {
+    toogleIsBreaking();
+    const tags = Array.prototype.slice.call(letterTags);
+
+    tags.forEach((tag, index) => {
+      intervalsIds[index] = setInterval(() => {
+        tag.innerHTML = generateRandomCharacters();
+      }, 80);
+
+      setTimeout(() => {
+        handleBreakingStop(index);
+
+        if (index === tags.length - 1) {
+          toogleIsBreaking();
+          missionTitle.innerHTML = "Mission: Completed!";
+          setButtonTitle("Decrypt");
+        }
+      }, calculateTimeout(index));
+    });
+  };
+
+  const handleBreakingStop = (index) => {
+    clearInterval(intervalsIds[index]);
+    restartIndex(index);
+  };
+
+  onClick(() => {
+    if (isLoadind()) {
+      return;
+    }
+
+    handleBreakingStart();
+    setButtonTitle("Decrypting...");
+  }, "decryptButton");
 });
